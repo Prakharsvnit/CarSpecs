1a965c8216a8fe772738bae096a68f7d34b06bad
Prakhar tripathi <tprakhar468@gmail.com>
2023-05-12 17:27:43 +0530

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const baseURL = "http://vaibhavarora2-001-site8.itempurl.com/api/vehicles/"
+export const baseURL = "https://vaibhavarora2-001-site8.itempurl.com/api/vehicles/"
 
 export const VehicleCategories = [{Category:"Volvo"},{Category:"BMW"},{Category:"Audi"},{Category:"Ford"},{Category:"Volkswagen"},]
